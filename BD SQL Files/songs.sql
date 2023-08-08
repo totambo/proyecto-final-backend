71f2c8f371c999173b31d681e01811b9b7f242d4
JestyUY <104555840+JestyUY@users.noreply.github.com>
2023-08-09 05:27:52 +1200

    genre.sql updated

---
+++ BD SQL Files/songs.sql
@@ -0,0 +1,67 @@
+INSERT INTO song (title, artist, duration, image)
+VALUES (
+        'Bohemian Rhapsody',
+        'Queen',
+        3 :55,
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/0/03/Queen_-_Bohemian_Rhapsody.jpg/1200px-Queen_-_Bohemian_Rhapsody.jpg'
+    ),
+    (
+        'Like a Virgin',
+        'Madonna',
+        3 :57,
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Madonna_-_Like_a_Virgin.jpg/1200px-Madonna_-_Like_a_Virgin.jpg'
+    ),
+    (
+        'Thriller',
+        'Michael Jackson',
+        4 :58,
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f1/Michael_Jackson_-_Thriller.jpg/1200px-Michael_Jackson_-_Thriller.jpg'
+    ),
+    (
+        'Billie Jean',
+        'Michael Jackson',
+        4 :53,
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/8/80/Michael_Jackson_-_Billie_Jean.jpg/1200px-Michael_Jackson_-_Billie_Jean.jpg'
+    ),
+    (
+        'I Want It That Way',
+        'Backstreet Boys',
+        3 :34,
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4d/Backstreet_Boys_-_I_Want_It_That_Way.jpg/1200px-Backstreet_Boys_-_I_Want_It_That_Way.jpg'
+    ),
+    (
+        'Don' t Stop Believin '',
+        'Journey',
+        4 :50,
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/4/43/Journey_-_Don%27t_Stop_Believing.jpg/1200px-Journey_-_Don%27t_Stop_Believing.jpg'
+    ),
+    (
+        'Sweet Caroline',
+        'Neil Diamond',
+        4 :47,
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c6/Neil_Diamond_-_Sweet_Caroline.jpg/1200px-Neil_Diamond_-_Sweet_Caroline.jpg'
+    ),
+    (
+        'Hallelujah',
+        'Leonard Cohen',
+        4 :34,
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/9/94/Leonard_Cohen_-_Hallelujah.jpg/1200px-Leonard_Cohen_-_Hallelujah.jpg'
+    ),
+    (
+        'Respect',
+        'Aretha Franklin',
+        2 :58,
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5e/Aretha_Franklin_-_Respect.jpg/1200px-Aretha_Franklin_-_Respect.jpg'
+    ),
+    (
+        'Imagine',
+        'John Lennon',
+        3 :05,
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/John_Lennon_-_Imagine.jpg/1200px-John_Lennon_-_Imagine.jpg'
+    ),
+    (
+        'Yesterday',
+        'The Beatles',
+        2 :03,
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/4/45/The_Beatles_-_Yesterday.jpg/1200px-The_Beatles_-_Yesterday.jpg'
+    );
